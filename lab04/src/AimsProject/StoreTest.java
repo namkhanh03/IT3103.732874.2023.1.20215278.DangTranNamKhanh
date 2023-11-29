2f15fb5441434c7788eb4322c166f4840ff88730
namkhanh03 <tnkhanhs03@gmail.com>
2023-11-29 19:45:11 +0700

    update Media

--- lab04/src/AimsProject/StoreTest.java
+++ lab04/src/AimsProject/StoreTest.java
@@ -4,6 +4,7 @@ import AimsProject.Media.DigitalVideoDisc;
 
 public class StoreTest {
     public static void main(String[] args) {
+        //
         Store store = new Store();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc(1,"The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         store.addMedia(dvd1);
@@ -14,5 +15,6 @@ public class StoreTest {
         DigitalVideoDisc dvd3 = new DigitalVideoDisc( 3,"Animation","Aladin", 18.99f);
         store.addMedia(dvd3);
         store.removeMedia(dvd2);
+
     }
 }
